f9fcaf0be7f4d14e4eec52f664104605c9534edd
sht02048 <sht02048@gmail.com>
2024-03-11 16:51:04 +0900

    Refactor: Intro

--- src/game/Intro.js
+++ src/game/Intro.js
@@ -1,23 +1,25 @@
 import Sprite from "./Sprite";
+import { IMAGE_PATH } from "../constants/path";
 
 class Intro {
   #floatSpeed = 0.1;
 
-  constructor(titlePath, instructionPath, game) {
+  constructor(game) {
     this.game = game;
     this.x = this.game.introCanvas.width / 2;
-    this.y = 0;
-    this.title = new Sprite(titlePath);
-    this.instruction = new Sprite(instructionPath);
+    this.titleY = 100;
+    this.instructionY = this.game.introCanvas.height - 150;
+    this.title = new Sprite(IMAGE_PATH.TITLE);
+    this.instruction = new Sprite(IMAGE_PATH.INSTRUCTION_START);
     this.isUp = true;
     this.amplitude = 0;
   }
 
   float() {
     if (this.isUp) {
-      this.y += this.#floatSpeed;
+      this.instructionY += this.#floatSpeed;
     } else {
-      this.y -= this.#floatSpeed;
+      this.instructionY -= this.#floatSpeed;
     }
 
     this.amplitude += 1;
@@ -26,31 +28,18 @@ class Intro {
       this.amplitude = 0;
       this.isUp = !this.isUp;
     }
-
-    this.game.introCtx.drawImage(
-      this.title,
-      this.x - this.title.width / 4,
-      100,
-      this.title.width / 2,
-      this.title.height / 2,
-    );
-
-    this.game.introCtx.drawImage(
-      this.instruction,
-      this.x - this.instruction.width / 4,
-      this.y + this.game.introCanvas.height - 150,
-      this.instruction.width / 2,
-      this.instruction.height / 2,
-    );
   }
 
   out() {
-    this.y += this.inAndOutSpeed;
+    this.titleY += this.game.inAndOutSpeed;
+    this.instructionY += this.game.inAndOutSpeed;
+  }
 
+  render() {
     this.game.introCtx.drawImage(
       this.title,
       this.x - this.title.width / 4,
-      this.y + 100,
+      this.titleY,
       this.title.width / 2,
       this.title.height / 2,
     );
@@ -58,7 +47,7 @@ class Intro {
     this.game.introCtx.drawImage(
       this.instruction,
       this.x - this.instruction.width / 4,
-      this.y + this.game.introCanvas.height - 150,
+      this.instructionY,
       this.instruction.width / 2,
       this.instruction.height / 2,
     );
