e5b2e5f1d49c4f7d039e848241b40c5b3448ec9b
sht02048 <sht02048@gmail.com>
2024-03-14 17:26:35 +0900

    Refactor: combine Sprite and part of Game into Renderer

---
+++ src/game/Renderer.js
@@ -0,0 +1,40 @@
+class Renderer {
+  #leftBlockSize = 100;
+  #rightBlockSize = 145;
+  #playerShipHeight = 61;
+  #mainCanvas = document.getElementById("main-canvas");
+  #introCanvas = document.getElementById("intro-canvas");
+
+  constructor(imagePath, width, height) {
+    this.imagePath = imagePath;
+    this.image = new Image();
+
+    this.width = width;
+    this.height = height;
+
+    this.image.src = this.imagePath;
+
+    this.mainCtx = this.#mainCanvas.getContext("2d");
+    this.introCtx = this.#introCanvas.getContext("2d");
+    this.canvasWidth = this.#mainCanvas.width;
+    this.canvasHeight = this.#mainCanvas.height;
+    this.minX = this.#leftBlockSize;
+    this.maxX = this.canvasWidth - this.#rightBlockSize;
+    this.minY = -10;
+    this.maxY = this.canvasHeight - this.#playerShipHeight + 5;
+    this.inAndOutSpeed = 4;
+    this.frame = 0;
+  }
+
+  render(x, y, width, height) {
+    if (width !== undefined && height !== undefined) {
+      this.mainCtx.drawImage(this.image, x, y, width, height);
+
+      return;
+    }
+
+    this.mainCtx.drawImage(this.image, x, y);
+  }
+}
+
+export default Renderer;
