292f8e745c73136813df5f2f9587607ae2f96139
sht02048 <sht02048@gmail.com>
2024-03-19 14:54:41 +0900

    Feat: explosion sound effect

--- src/graphics/Explosion.js
+++ src/graphics/Explosion.js
@@ -1,25 +1,35 @@
 import Renderer from "./Renderer";
-import { ENEMY_EXPLOSION } from "../constants/path";
+import Sound from "../utils/Sound";
+import { ENEMY_EXPLOSION, AUDIO } from "../constants/path";
 
 class Explosion {
   #enemyExplosionList = [];
 
   constructor(game) {
     this.game = game;
+    this.explosionFrame = 0;
+    this.explosionSound = new Sound(AUDIO.EXPLOSION);
+    this.explosionSound.sound.loop = false;
+    this.explosionSound.sound.volume = 0.3;
+    this.isExplosionSoundPlaying = false;
 
     for (let i = 1; i < 12; i += 1) {
       this.#enemyExplosionList.push(
         new Renderer(`${ENEMY_EXPLOSION}enemy_${i}.png`),
       );
     }
-
-    this.explosionFrame = 0;
   }
 
   destroy(x, y, width) {
     if (this.explosionFrame === 44) {
       return;
     }
+
+    if (!this.isExplosionSoundPlaying) {
+      this.explosionSound.playAudio();
+    }
+
+    this.isExplosionSoundPlaying = true;
 
     const currentFrame = Math.floor(this.explosionFrame / 4);
 
