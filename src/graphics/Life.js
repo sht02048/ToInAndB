a3b91b85baf805a756bba80c8c212298e00ead16
sht02048 <sht02048@gmail.com>
2024-03-19 21:35:23 +0900

    Feat: add lifeBoard

---
+++ src/graphics/Life.js
@@ -0,0 +1,26 @@
+import Renderer from "./Renderer";
+import { PLATER } from "../constants/path";
+
+class LifeBoard {
+  #currentHealth = 2;
+
+  constructor() {
+    this.healthList = [];
+
+    for (let i = 0; i < this.#currentHealth; i += 1) {
+      this.healthList.push(new Renderer(PLATER.SPAWN));
+    }
+  }
+
+  render(playerHealth) {
+    this.healthList.forEach((health, index) => {
+      if (playerHealth - 1 <= index) {
+        return;
+      }
+
+      health.render(health.minX + index * 40 + 20, 20);
+    });
+  }
+}
+
+export default LifeBoard;
