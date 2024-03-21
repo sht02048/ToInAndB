32253c12435e20e4f11536c1d4ccf94f932db6bf
sht02048 <sht02048@gmail.com>
2024-03-21 21:56:17 +0900

    Rename: change assets path

--- src/constants/path.js
+++ src/constants/path.js
@@ -1,76 +1,76 @@
 const PLATER = {
-  STATIC: "src/assets/sprites/player/player_static.png",
-  STRAIGHT: "src/assets/sprites/player/player_straight.png",
-  LEFT: "src/assets/sprites/player/player_left.png",
-  RIGHT: "src/assets/sprites/player/player_right.png",
-  LIFE: "src/assets/sprites/player/player_life.png",
+  STATIC: "assets/sprites/player/player_static.png",
+  STRAIGHT: "assets/sprites/player/player_straight.png",
+  LEFT: "assets/sprites/player/player_left.png",
+  RIGHT: "assets/sprites/player/player_right.png",
+  LIFE: "assets/sprites/player/player_life.png",
 };
 
 const ENEMIES = {
-  HEAVY: "src/assets/sprites/enemies/heavy.png",
-  HEAVY_HIT: "src/assets/sprites/enemies/heavy_hit.png",
-  BUG: "src/assets/sprites/enemies/bug.png",
-  BUG_HIT: "src/assets/sprites/enemies/bug_hit.png",
-  MINI: "src/assets/sprites/enemies/mini.png",
-  MINI_HIT: "src/assets/sprites/enemies/mini_hit.png",
-  CANNON: "src/assets/sprites/enemies/cannon.png",
-  CANNON_HIT: "src/assets/sprites/enemies/cannon_hit.png",
-  DANGER: "src/assets/sprites/enemies/danger.png",
-  DANGER_HIT: "src/assets/sprites/enemies/danger_hit.png",
-  WINGS: "src/assets/sprites/enemies/wings.png",
-  WINGS_HIT: "src/assets/sprites/enemies/wings_hit.png",
-  CROSS: "src/assets/sprites/enemies/cross.png",
-  CROSS_HIT: "src/assets/sprites/enemies/cross_hit.png",
-  COW: "src/assets/sprites/enemies/cow.png",
-  COW_HIT: "src/assets/sprites/enemies/cow_hit.png",
-  EMPEROR: "src/assets/sprites/enemies/emperor.png",
-  EMPEROR_HIT: "src/assets/sprites/enemies/emperor_hit.png",
+  HEAVY: "assets/sprites/enemies/heavy.png",
+  HEAVY_HIT: "assets/sprites/enemies/heavy_hit.png",
+  BUG: "assets/sprites/enemies/bug.png",
+  BUG_HIT: "assets/sprites/enemies/bug_hit.png",
+  MINI: "assets/sprites/enemies/mini.png",
+  MINI_HIT: "assets/sprites/enemies/mini_hit.png",
+  CANNON: "assets/sprites/enemies/cannon.png",
+  CANNON_HIT: "assets/sprites/enemies/cannon_hit.png",
+  DANGER: "assets/sprites/enemies/danger.png",
+  DANGER_HIT: "assets/sprites/enemies/danger_hit.png",
+  WINGS: "assets/sprites/enemies/wings.png",
+  WINGS_HIT: "assets/sprites/enemies/wings_hit.png",
+  CROSS: "assets/sprites/enemies/cross.png",
+  CROSS_HIT: "assets/sprites/enemies/cross_hit.png",
+  COW: "assets/sprites/enemies/cow.png",
+  COW_HIT: "assets/sprites/enemies/cow_hit.png",
+  EMPEROR: "assets/sprites/enemies/emperor.png",
+  EMPEROR_HIT: "assets/sprites/enemies/emperor_hit.png",
 };
 
 const BACKGROUNDS = {
-  BASE: "src/assets/sprites/backgrounds/base.png",
-  BLOCK: "src/assets/sprites/backgrounds/block.png",
-  PLATE: "src/assets/sprites/backgrounds/plate.png",
-  SPACE: "src/assets/sprites/backgrounds/space.png",
+  BASE: "assets/sprites/backgrounds/base.png",
+  BLOCK: "assets/sprites/backgrounds/block.png",
+  PLATE: "assets/sprites/backgrounds/plate.png",
+  SPACE: "assets/sprites/backgrounds/space.png",
 };
 
 const IMAGE = {
-  TITLE: "src/assets/images/title.png",
-  INSTRUCTION_START: "src/assets/images/instruction_start.png",
-  PAUSED: "src/assets/images/paused.png",
-  RESUME: "src/assets/images/resume.png",
-  REPLAY: "src/assets/images/replay.png",
-  GAME_OVER: "src/assets/images/game_over.png",
+  TITLE: "assets/images/title.png",
+  INSTRUCTION_START: "assets/images/instruction_start.png",
+  PAUSED: "assets/images/paused.png",
+  RESUME: "assets/images/resume.png",
+  REPLAY: "assets/images/replay.png",
+  GAME_OVER: "assets/images/game_over.png",
 };
 
 const AUDIO = {
-  BATTLE: "src/assets/audios/background/pixel-perfect.mp3",
-  INTRO: "src/assets/audios/background/battle.mp3",
-  EXPLOSION: "src/assets/audios/effects/explosion_1.mp3",
-  SHOT: "src/assets/audios/effects/shot_1.mp3",
-  POWER_UP: "src/assets/audios/effects/power_up.mp3",
-  SPEED_UP: "src/assets/audios/effects/speed_up.mp3",
+  BATTLE: "assets/audios/background/pixel-perfect.mp3",
+  INTRO: "assets/audios/background/battle.mp3",
+  EXPLOSION: "assets/audios/effects/explosion_1.mp3",
+  SHOT: "assets/audios/effects/shot_1.mp3",
+  POWER_UP: "assets/audios/effects/power_up.mp3",
+  SPEED_UP: "assets/audios/effects/speed_up.mp3",
 };
 
 const PROJECTILE = {
-  LEVEL_1: "src/assets/images/projectile_1.png",
-  LEVEL_2: "src/assets/images/projectile_2.png",
-  LEVEL_3: "src/assets/images/projectile_3.png",
-  GUIDED: "src/assets/images/projectile_4.png",
+  LEVEL_1: "assets/images/projectile_1.png",
+  LEVEL_2: "assets/images/projectile_2.png",
+  LEVEL_3: "assets/images/projectile_3.png",
+  GUIDED: "assets/images/projectile_4.png",
 };
 
 const ENEMY_PROJECTILE = {
-  NORMAL: "src/assets/images/projectile_1_enemy.png",
-  AIMED: "src/assets/images/projectile_2_enemy.png",
-  GUIDED: "src/assets/images/projectile_3_enemy.png",
-  BOOMERANG: "src/assets/images/projectile_4_enemy.png",
+  NORMAL: "assets/images/projectile_1_enemy.png",
+  AIMED: "assets/images/projectile_2_enemy.png",
+  GUIDED: "assets/images/projectile_3_enemy.png",
+  BOOMERANG: "assets/images/projectile_4_enemy.png",
 };
 
-const ENEMY_EXPLOSION = "src/assets/images/explosion_";
+const ENEMY_EXPLOSION = "assets/images/explosion_";
 
 const ITEM_IMAGE = {
-  POWER_UP: "src/assets/images/power_up.png",
-  SPEED_UP: "src/assets/images/speed_up.png",
+  POWER_UP: "assets/images/power_up.png",
+  SPEED_UP: "assets/images/speed_up.png",
 };
 
 export {
