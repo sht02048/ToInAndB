939a1ad8bcacbc9bcf2cbdb6fe547bb6fc4155c1
sht02048 <sht02048@gmail.com>
2024-05-17 15:43:30 +0900

    Setting: typescript migration eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,26 @@
 module.exports = {
+    "parser": "@typescript-eslint/parser",
     "env": {
         "browser": true,
         "es2021": true
     },
+    "plugins": [
+        "@typescript-eslint"
+    ],
     "extends": [
         "airbnb-base",
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:import/recommended",
         "plugin:prettier/recommended"
     ],
+    "settings": {
+        "import/resolver": {
+          "node": {
+            "extensions": [".js", ".jsx", ".ts", ".tsx"]
+          }
+        }
+    },
     "overrides": [
         {
             "env": {
@@ -21,10 +35,24 @@ module.exports = {
         }
     ],
     "parserOptions": {
-        "sourceType": "module"
+        "sourceType": "module",
+        "ecmaVersion": 13,
+        "project": "./tsconfig.json"
     },
     "rules": {
         "semi": ["error", "always"],
         "quotes": ["error", "double"],
+        "lines-between-class-members": ["error", "always", { "exceptAfterSingleLine": true }],
+        "import/extensions": [
+            "error",
+            "ignorePackages",
+            {
+              "": "never",
+              "js": "never",
+              "jsx": "never",
+              "ts": "never",
+              "tsx": "never"
+            }
+         ]
     }
 }
