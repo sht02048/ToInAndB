1783aece89cf859d1dbbeb4f97c137e4bcb14339
sht02048 <sht02048@gmail.com>
2024-05-17 17:41:43 +0900

    Refactor: load image when sprite is rendered

--- src/graphics/Renderer.ts
+++ src/graphics/Renderer.ts
@@ -26,11 +26,12 @@ class Renderer {
 
   constructor(imagePath?: string) {
     this.imagePath = imagePath;
-    this.image = new Image();
-    this.image.src = this.imagePath || "";
-
-    this.mainCanvas = document.getElementById("main-canvas") as HTMLCanvasElement;
-    this.introCanvas = document.getElementById("intro-canvas") as HTMLCanvasElement;
+    this.mainCanvas = document.getElementById(
+      "main-canvas",
+    ) as HTMLCanvasElement;
+    this.introCanvas = document.getElementById(
+      "intro-canvas",
+    ) as HTMLCanvasElement;
     this.mainCtx = this.mainCanvas.getContext("2d");
     this.introCtx = this.introCanvas.getContext("2d");
     this.canvasWidth = this.mainCanvas.width;
@@ -43,33 +44,47 @@ class Renderer {
     this.maxY = this.canvasHeight - this.#playerShipHeight + 5;
     this.inAndOutSpeed = 4;
     this.frame = 0;
+  }
 
-    this.image.onload = () => {
-      this.width = this.image.width;
-      this.height = this.image.height;
-    };
+  private loadImage(): HTMLImageElement {
+    if (!this.image) {
+      this.image = new Image();
+      this.image.src = this.imagePath || "";
+      this.image.onload = () => {
+        this.width = this.image!.width;
+        this.height = this.image!.height;
+      };
+    }
+    return this.image;
   }
 
   render(x: number, y: number, width?: number, height?: number): void {
-    if (width !== undefined && height !== undefined) {
-      this.mainCtx.drawImage(this.image, x, y, width, height);
-
-      return;
+    const img = this.loadImage();
+    if (width && height) {
+      this.mainCtx.drawImage(img, x, y, width, height);
+    } else {
+      this.mainCtx.drawImage(img, x, y);
     }
-
-    this.mainCtx.drawImage(this.image, x, y);
   }
 
-  renderAngle(x: number, y: number, width: number, height: number, angle: number): void {
+  renderAngle(
+    x: number,
+    y: number,
+    width: number,
+    height: number,
+    angle: number,
+  ): void {
+    const img = this.loadImage();
     this.mainCtx.save();
-    this.mainCtx.translate(x, y + 10);
+    this.mainCtx.translate(x + width / 2, y + height / 2);
     this.mainCtx.rotate(angle);
-    this.mainCtx.drawImage(this.image, -width, -height, width, height);
+    this.mainCtx.drawImage(img, -width / 2, -height / 2, width, height);
     this.mainCtx.restore();
   }
 
-  renderIntro(x: number, y: number, width: number, height: number): void  {
-    this.introCtx.drawImage(this.image, x, y, width, height);
+  renderIntro(x: number, y: number, width: number, height: number): void {
+    const img = this.loadImage();
+    this.introCtx.drawImage(img, x, y, width, height);
   }
 }
 
