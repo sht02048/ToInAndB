73bb74a38a16ad8308700be7a8f63efedf24d610
sht02048 <sht02048@gmail.com>
2024-05-24 17:01:53 +0900

    Setting: enable import images in ts file

---
+++ types/global.d.ts
@@ -0,0 +1,16 @@
+/// <reference types="vite/client" />
+
+declare module "*.png" {
+  const value: any;
+  export { value };
+}
+
+declare module "*.mp3" {
+  const value: any;
+  export { value };
+}
+
+declare module "*.mp4" {
+  const value: any;
+  export { value };
+}
